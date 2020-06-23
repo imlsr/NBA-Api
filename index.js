f32b39ebad2c693719b7138c20a92b2dfdefd270
imlsr <likhith.sr@studentpartner.com>
2020-06-23 19:43:38 +0530

    Test

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/",function(req,res){
+  // console.log(req.body);
+  request("https://www.balldontlie.io/api/v1/players?search=lebron", function(error , response , body){
+    let data = JSON.parse(body);
+
+
+
+  })
+  console.log(data);
+})
+
+
+
+
+
+
+
+
+
 
 app.listen(3000,function(){
   console.log("Server Running");
