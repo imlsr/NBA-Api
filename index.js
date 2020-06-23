378d4beabd0159226624029ea37821c6e0156bb4
imlsr <likhith.sr@studentpartner.com>
2020-06-23 20:17:44 +0530

    Added EJS views

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const ejs = require("ejs");
 
 const app = express();
 
+app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
 app.get("/",function(req,res){
-  res.sendFile(__dirname + "/index.html");
+  res.render('index');
 })
 
 app.post("/",function(req,res){
   // console.log(req.body);
   request("https://www.balldontlie.io/api/v1/players?search=lebron", function(error , response , body){
-    let data = JSON.parse(body);
+    let d = JSON.parse(body);
+    res.render('result',{
+      FirstName :d.data[0].first_name,
+      LastName :d.data[0].last_name
 
-
+    })
 
   })
-  console.log(data);
+
 })
 
 
