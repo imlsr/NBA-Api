d8b58e11c10bd32117c2a2c4819630a88984e122
imlsr <likhith.sr@studentpartner.com>
2020-06-25 20:12:03 +0530

    Added info

--- index.js
+++ index.js
@@ -14,12 +14,18 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
-  // console.log(req.body);
-  request("https://www.balldontlie.io/api/v1/players?search=lebron", function(error , response , body){
+  let PlayerName = req.body.player;
+  let Urlmain = "https://www.balldontlie.io/api/v1/players?search=";
+  request(Urlmain + PlayerName, function(error , response , body){
     let d = JSON.parse(body);
     res.render('result',{
       FirstName :d.data[0].first_name,
-      LastName :d.data[0].last_name
+      LastName :d.data[0].last_name,
+      HeightFeet:d.data[0].height_feet,
+      HeightInches:d.data[0].height_inches,
+      Team: d.data[0].team.full_name,
+      Weight:d.data[0].weight_pounds,
+      Position:d.data[0].position
 
     })
 
